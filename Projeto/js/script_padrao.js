1554a150cc3a11957bfef669e5a293f46121b68c
Henrique Arantes <henriquea945@gmail.com>
2020-12-08 21:42:32 -0300

    Adição da página "contatos"

--- Projeto/js/script_padrao.js
+++ Projeto/js/script_padrao.js
@@ -1,3 +1,15 @@
 function menuHamburguer(x) {
-    x.classList.toggle("clickMenu");
-  }
+  x.classList.toggle("clickMenu");
+}
+
+function confirma(){
+  window.alert("Enviado Com Sucesso");
+}
+
+function comp1(){
+  document.contato.c_nome.value = document.contato.p_nome.value;
+}
+
+function comp2(){
+  document.contato.c_nome.value = document.contato.c_nome.value + " " + document.contato.u_nome.value;
+}
